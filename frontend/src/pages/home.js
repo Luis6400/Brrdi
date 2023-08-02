9d6d6434a84c94f95ee6005f19dc5ffa03a7888a
Dane Kargis <dane.kargis@gmail.com>
2023-08-01 21:21:31 -0600

    started front end

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Home = () => {
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <div className="col-12 col-md-8 mb-3">
+          <header className="mb-5">
+
+            </header>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
