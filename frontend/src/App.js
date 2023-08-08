53399445aff2d11579dbdbb41e0bb56acf5a826c
Dane Kargis <dane.kargis@gmail.com>
2023-08-07 20:27:18 -0600

    login fix from adam

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
 import Home from './pages/home';
 import Login from './pages/loginsignup';
-
 import './App.css';
-
-
+// Set up Apollo Client
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:3001/graphql' }),
+  cache: new InMemoryCache(),
+});
 function App() {
-
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />     
-        
-      </Routes>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
-
 export default App;
