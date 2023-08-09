4109b6061129c8d89439e14c41f8a31fde9a3cb9
Dane Kargis <dane.kargis@gmail.com>
2023-08-08 20:16:07 -0600

    letting luis cook

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import React from 'react';
 import DefaultSidebar from '../components/nav';
 import Feed from '../components/feed';
 import Adspace from '../components/adspace';
 import Profile from './profile';
 import SearchBar from './searchbar';
-
+import { useLocation } from 'react-router-dom';
 const Layout = (PropsWithChildren) => {
+    const {pathname} = useLocation();
+    const handleSelected = () => {
+        const selected = localStorage.getItem("selected");
+        // if (!localStorage.getItem("selected")) {
+        //      const selected = 1;
+        //  }
+
+        switch (selected) {
+            case 1:
+                return <Feed/>;
+            case 2:
+                return <Profile/>;
+            case 3:
+                return <SearchBar/>;
+            default:
+                return <Feed/>;
+        }
+    };
     return (
         <div class="grid grid-cols-10 gap-4">
-            <div class="col-span-3 relative"><DefaultSidebar/></div>
-            <div class="col-span-4"><SearchBar/><Profile/><Feed/></div>
+            <div class="col-span-3 relative"><DefaultSidebar onClick = {(handleSelected)} /></div>
+            {/* <div class="col-span-4">{handleSelected()}</div> */}
+            {pathname.includes("/profile") && <Profile/>}
+            {pathname.includes("/search") && <SearchBar/>}
+            {pathname.includes("/feed") && <Feed/>}
             <div class="col-span-3 relative"><Adspace/></div>
         </div>
     );
